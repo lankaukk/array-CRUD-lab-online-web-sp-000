09cfff52198935145806e817c750ad7336854f0d
McKayla Lankau <lankaukk@yahoo.com>
2020-07-28 17:53:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,7 @@ def remove_element_from_end_of_array(new_array)
   last_element = new_array.pop
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(new_array)
   
 end
 
