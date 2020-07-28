dabca5773d6ec01317fdaa0e00de7331f7822ee8
McKayla Lankau <lankaukk@yahoo.com>
2020-07-28 17:26:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,7 +6,8 @@ def create_an_array
   new_array = [1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(new_array, "five")
+  new_array << "five"
   
 end
 
