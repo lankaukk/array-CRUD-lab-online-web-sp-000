d053f7752f2146db2b4af534c681153eff4fb7c6
McKayla Lankau <lankaukk@yahoo.com>
2020-07-28 17:50:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 
 def remove_element_from_end_of_array(new_array)
   new_array = [1,2,3,4, "arrays!"]
-  new_array.pop(element) 
+  new_array.pop("arrays!") 
 end
 
 def remove_element_from_start_of_array(array)
